4325ab298a17f2ce99acbbe75f057dea6586d57d
leminlimez <jirizarry317@gmail.com>
2023-10-13 23:26:19 -0400

    move widget customization view to its own file

---
+++ src/views/WidgetCustomizationView.swift
@@ -0,0 +1,22 @@
+//
+//  WidgetCustomizationView.swift
+//  
+//
+//  Created by lemin on 10/13/23.
+//
+
+import Foundation
+import SwiftUI
+
+// MARK: Widget Customization View
+// TODO: This
+struct WidgetCustomizationView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("TODO")
+            }
+            .navigationTitle("Customize")
+        }
+    }
+}
