5661f46c334c242ce83daa5a4bb37c5b9c89dc95
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-10-18 14:13:12 -0400

    start the widget modification view

--- src/views/WidgetCustomizationView.swift
+++ src/views/WidgetCustomizationView.swift
@@ -1,6 +1,6 @@
 //
 //  WidgetCustomizationView.swift
-//  
+//
 //
 //  Created by lemin on 10/13/23.
 //
@@ -14,9 +14,132 @@ struct WidgetCustomizationView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Text("TODO")
+                WidgetPlaceView(widgetManager: .init(widgetSide: .center))
             }
             .navigationTitle("Customize")
         }
+    }
+}
+
+// MARK: Widget Place View
+struct WidgetPlaceView: View {
+    @State var canAdd: Bool = true
+    @StateObject var widgetManager: WidgetManager
+    @State var viewIndex: Int = -1
+    @State var showingAddView: Bool = false
+    @State var showingModView: Bool = false
+
+    var body: some View {
+        HStack {
+            HStack {
+                ForEach (0..<widgetManager.widgetStructs.count, id: \.self) { widget in
+                    Button(action: {
+                        viewIndex = widget
+                        showingModView.toggle()
+                    }) {
+                        WidgetPreviewsView(widget: $widgetManager.widgetStructs[widget])
+                    }
+                }
+                .padding(.horizontal, 1)
+                if canAdd {
+                    // add a plus button
+                    Button(action: {
+                        showingAddView.toggle()
+                    }) {
+                        Image(systemName: "plus")
+                            .padding(.horizontal, 10)
+                            .padding(.vertical, 3)
+                    }
+                    .background(.ultraThinMaterial, in:
+                                    RoundedRectangle(cornerRadius: 100.0))
+                    .sheet(isPresented: $showingAddView, content: {
+                        WidgetAddView(widgetManager: widgetManager)
+                    })
+                    .sheet(isPresented: $showingModView, content: {
+                        WidgetModifyView(widgetManager: widgetManager, widgetIndex: $viewIndex)
+                    })
+                }
+            }
+            .padding(.horizontal, 5)
+        }
+        .background(.ultraThinMaterial, in:
+            RoundedRectangle(cornerRadius: 8))
+    }
+}
+
+// MARK: Widget Modify View
+struct WidgetModifyView: View {
+    @Environment(\.dismiss) var dismiss
+    @StateObject var widgetManager: WidgetManager
+    @Binding var widgetIndex: Int
+    
+    var body: some View {
+        VStack {
+            Text("Modify Widget")
+                .bold()
+                .font(.title3)
+            // Widget Preferences
+            WidgetPreferencesView(widgetStruct: $widgetManager.widgetStructs[widgetIndex])
+            // Save Button
+            Button("Save") {
+                widgetManager.saveWidgetStructs()
+                dismiss()
+            }
+            .buttonStyle(TintedButton(color: .blue))
+            // Delete Button
+            Button("Delete") {
+                widgetManager.removeWidget(id: widgetIndex)
+                dismiss()
+            }
+            .buttonStyle(TintedButton(color: .red))
+        }
+    }
+}
+
+// MARK: Widget Add View
+struct WidgetAddView: View {
+    @Environment(\.dismiss) var dismiss
+    @StateObject var widgetManager: WidgetManager
+    
+    var body: some View {
+        List {
+            ForEach(WidgetModule.allCases, id: \.self) { id in
+                Button(action: {
+                    widgetManager.addWidget(module: id)
+                    dismiss()
+                }) {
+                    WidgetChoiceView(widgetName: WidgetDetails.getWidgetName(id), exampleText: WidgetDetails.getWidgetExample(id))
+                }
+            }
+        }
+    }
+}
+
+// MARK: Widget Choice View
+struct WidgetChoiceView: View {
+    var widgetName: String
+    var exampleText: String
+
+    var body: some View {
+        HStack {
+            ZStack {
+                Text(exampleText)
+                    .padding(.vertical, 5)
+                    .foregroundColor(.secondary)
+            }
+            .frame(width: 125)
+            .background(.ultraThinMaterial, in:
+                RoundedRectangle(cornerRadius: 8))
+            .padding(.trailing, 5)
+            Text(widgetName)
+                .foregroundColor(.primary)
+                .bold()
+        }
+    }
+}
+
+struct WidgetCustomizationView_Previews: PreviewProvider {
+    static var previews: some View {
+        WidgetCustomizationView()
     }
 }
---
+++ src/views/widget/WidgetPreferencesView.swift
@@ -0,0 +1,96 @@
+//
+//  WidgetPreferencesView.swift
+//  Helium UI
+//
+//  Created by lemin on 10/18/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct WidgetPreferencesView: View {
+    @Binding var widgetStruct: WidgetStruct
+    @State var text: String = ""
+    @State var intSelection: Int = 0
+    
+    var body: some View {
+        VStack {
+            switch (widgetStruct.module) {
+            case .date:
+                // MARK: Date Format Textbox
+                HStack {
+                    Text("Date Format")
+                        .padding(.trailing, 10)
+                    TextField("E MMM dd", text: $text)
+                        .frame(maxWidth: 120)
+                        .multilineTextAlignment(.trailing)
+                        .onAppear {
+                            if let format = widgetStruct.config["dateFormat"] as? String {
+                                text = format
+                            } else {
+                                text = "E MMM dd"
+                            }
+                        }
+                }
+            case .network:
+                // MARK: Network Choice
+                Picker(selection: $intSelection, label: Text("Network Type")) {
+                    Text("Down").tag(0)
+                    Text("Up").tag(1)
+                }
+                .onAppear {
+                    if let netUp = widgetStruct.config["isUp"] as? Bool {
+                        intSelection = netUp ? 1 : 0
+                    } else {
+                        intSelection = 0
+                    }
+                }
+            case .battery:
+                // MARK: Battery Value Type
+                Picker(selection: $intSelection, label: Text("Battery Option")) {
+                    Text("Watts").tag(0)
+                    Text("Charging Current").tag(1)
+                    Text("Amperage").tag(2)
+                    Text("Charge Cycles").tag(3)
+                }
+                .onAppear {
+                    if let batteryType = widgetStruct.config["batteryValueType"] as? Int {
+                        intSelection = batteryType
+                    } else {
+                        intSelection = 0
+                    }
+                }
+            default:
+                Text("No Configurable Aspects")
+            }
+        }
+        .padding(.horizontal, 15)
+        .onChange(of: text) { newText in
+            // MARK: Changing Text
+            switch(widgetStruct.module) {
+            case .date:
+                // MARK: Date Format Handling
+                if newText == "" {
+                    widgetStruct.config["dateFormat"] = nil
+                } else {
+                    widgetStruct.config["dateFormat"] = text
+                }
+            default:
+                return
+            }
+        }
+        .onChange(of: intSelection) { newInt in
+            // MARK: Changing Integer
+            switch(widgetStruct.module) {
+            case .network:
+                // MARK: Network Choice Handling
+                widgetStruct.config["isUp"] = newInt == 1 ? true : false
+            case .battery:
+                // MARK: Battery Value Type Handling
+                widgetStruct.config["batteryValueType"] = newInt
+            default:
+                return
+            }
+        }
+    }
+}
