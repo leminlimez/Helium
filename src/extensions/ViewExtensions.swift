1417a9d29af35ae175da715c742343c3b6ea8a1c
leminlimez <jirizarry317@gmail.com>
2023-10-19 13:33:42 -0400

    full width

--- src/extensions/ViewExtensions.swift
+++ src/extensions/ViewExtensions.swift
@@ -11,18 +11,29 @@ import SwiftUI
 // MARK: Tinted Button Style
 struct TintedButton: ButtonStyle {
     var color: Color
+    var fullWidth: Bool = false
     
     func makeBody(configuration: Configuration) -> some View {
         ZStack {
-            configuration.label
-                .padding(15)
-                .background(AnyView(color.opacity(0.2)))
-                .cornerRadius(8)
-                .foregroundColor(color)
+            if fullWidth {
+                configuration.label
+                    .padding(15)
+                    .frame(maxWidth: .infinity)
+                    .background(AnyView(color.opacity(0.2)))
+                    .cornerRadius(8)
+                    .foregroundColor(color)
+            } else {
+                configuration.label
+                    .padding(15)
+                    .background(AnyView(color.opacity(0.2)))
+                    .cornerRadius(8)
+                    .foregroundColor(color)
+            }
         }
     }
     
-    init(color: Color = .blue) {
+    init(color: Color = .blue, fullWidth: Bool = false) {
         self.color = color
+        self.fullWidth = fullWidth
     }
 }
