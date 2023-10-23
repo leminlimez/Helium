b680d4dee60f3f7002d8146f180f4edc11ae4782
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-10-23 10:53:01 -0400

    large notch preferences

--- src/widgets/DeviceScaleManager.mm
+++ src/widgets/DeviceScaleManager.mm
@@ -10,8 +10,12 @@
 #import "DeviceScaleManager.h"
 
 // Small Notch Definitions
-#define SMALL_SIDE_WIDGET_SIZE 0.27435897
-#define SMALL_CENTER_WIDGET_SIZE 0.34615385
+#define SMALL_SIDE_WIDGET_SIZE 0.27435897   // Original Size (iPhone 13 Pro): 107
+#define SMALL_CENTER_WIDGET_SIZE 0.34615385 // Original Size (iPhone 13 Pro): 135
+
+// Large Notch Definitions
+#define LARGE_SIDE_WIDGET_SIZE 0.19466667   // Original Size (iPhone X): 73
+#define LARGE_CENTER_WIDGET_SIZE 0.50666667 // Original Size (iPhone X): 190
 
 NSString* getDeviceName()
 {
@@ -25,7 +29,7 @@ NSString* getDeviceName()
  Sizes:
  0 = No Notch
  1 = Small Notch
- 2 = Big Notch
+ 2 = Large Notch
  3 = Dynamic Island
  */
 NSInteger getDeviceSize()
@@ -34,8 +38,16 @@ NSInteger getDeviceSize()
     
     // get the notch size
     if ([model rangeOfString: @"iPhone14"].location != NSNotFound) {
-        // small notch
+        // Small Notch
         return 1;
+    } else if (
+        [model rangeOfString: @"iPhone10,3"].location != NSNotFound     // account for iPhone 8
+        || [model rangeOfString: @"iPhone10,6"].location != NSNotFound  // which is also iPhone10,_
+        || [model rangeOfString: @"iPhone11"].location != NSNotFound
+        || [model rangeOfString: @"iPhone12"].location != NSNotFound
+        || [model rangeOfString: @"iPhone13"].location != NSNotFound
+    ) {
+        return 2;
     }
     return 0;
 }
@@ -48,6 +60,9 @@ double getSideWidgetSize()
     if (deviceSize == 1) {
         // Small Notch
         return SMALL_SIDE_WIDGET_SIZE;
+    } else if (deviceSize == 2) {
+        // Large Notch
+        return LARGE_SIDE_WIDGET_SIZE;
     }
     
     return SMALL_SIDE_WIDGET_SIZE;
@@ -61,6 +76,9 @@ double getCenterWidgetSize()
     if (deviceSize == 1) {
         // Small Notch
         return SMALL_CENTER_WIDGET_SIZE;
+    } else if (deviceSize == 2) {
+        // Large Notch
+        return LARGE_CENTER_WIDGET_SIZE;
     }
     
     return SMALL_CENTER_WIDGET_SIZE;
@@ -77,6 +95,12 @@ NSDictionary<NSString*, NSNumber*>* getMaxNumWidgets()
         return @{
             @"sideNum": @(2),
             @"centerNum": @(3)
+        };
+    } else if (deviceSize == 2) {
+        // Large Notch
+        return @{
+            @"sideNum": @(1),
+            @"centerNum": @(4)
         };
     }
     
