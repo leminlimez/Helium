1e2b7440c893573249a1dafe1c8ff0253006d466
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-12-08 21:18:10 -0500

    loading widgets from array/dictionary (untested)

---
+++ src/extensions/UsefulFunctions.h
@@ -0,0 +1,14 @@
+//
+//  UsefulFunctions.h
+//  
+//
+//  Created by lemin on 12/8/23.
+//
+
+BOOL getBoolFromDictKey(NSDictionary *dict, NSString *key);
+
+NSInteger getIntFromDictKey(NSDictionary *dict, NSString *key, NSInteger defaultValue);
+NSInteger getIntFromDictKey(NSDictionary *dict, NSString *key);
+
+double getDoubleFromDictKey(NSDictionary *dict, NSString *key, double defaultValue);
+double getDoubleFromDictKey(NSDictionary *dict, NSString *key);
